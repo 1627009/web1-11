b54d156a6669689a595f860546ba5233241bc3af
1627009 <g1627009@st.t-kougei.ac.jp>
2017-06-19 17:42:08 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,7 +2,7 @@ var number = Math.floor(Math.random()*100);
 var result = 0;
 while(1)
 {
-  var answer = parseInt(window.prompt('数当てゲーム！0~99の数字を入力してください！'));
+  var answer = parseInt(window.prompt('数当てゲーム！0~99の数字を入力してください！\n%d',answer));
 
   var message;
   if(answer === number)
@@ -15,7 +15,7 @@ while(1)
   else if(answer > number)
     result = window.confirm('残念、もっと小さいよ\nこのまま続ける？');
   else
-    result = window.confirm('0~2の数字を入力してね\nこのまま続ける？');
+    result = window.confirm('0~99の数字を入力してね\nこのまま続ける？');
 
   document.getElementById('choice').textContent = message;
   
