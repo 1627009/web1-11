b79ecd688eac135178febb4ae79d5f455e9d8e04
1627009 <g1627009@st.t-kougei.ac.jp>
2017-06-19 17:18:09 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,22 +1,28 @@
 var number = Math.floor(Math.random()*3);
-var answer = parseInt(window.prompt('数当てゲーム！0~2の数字を入力してください！'));
+var trueFlag = FALSE;
 
-var message;
-if(answer === number)
+while(trueFlag === TRUE)
 {
-  message = 'あたり';
-}
-else if(answer < number)
-{
-  message = '残念、もっと大きいよ';
-}
-else if(answer > number)
-{
-  message = '残念、もっと小さいよ';
-}
-else
-{
-  message = '0~2の数字を入力してね';
-}
+  var answer = parseInt(window.prompt('数当てゲーム！0~2の数字を入力してください！'));
 
-document.getElementById('choice').textContent = message;
+  var message;
+  if(answer === number)
+  {
+    message = 'あたり';
+    trueFlag = TRUE;
+  }
+  else if(answer < number)
+  {
+    message = '残念、もっと大きいよ';
+  }
+  else if(answer > number)
+  {
+    message = '残念、もっと小さいよ';
+  }
+  else
+  {
+    message = '0~2の数字を入力してね';
+  }
+
+  document.getElementById('choice').textContent = message;
+}
